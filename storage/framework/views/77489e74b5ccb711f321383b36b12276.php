1794e97668f61f155f8d3b0c84b991c73524155f
waliullahcoder <waliullahbiplob786@gmail.com>
2025-11-21 15:16:46 +0600

    garbage removed

--- storage/framework/views/77489e74b5ccb711f321383b36b12276.php
+++
@@ -1,8 +0,0 @@
-<aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
-    <section class="sidebar">
-        <!-- sidebar menu: : style can be found in sidebar.less -->
-        <?php echo $__env->make('administrator.layouts.menu', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    </section>
-    <!-- /.sidebar -->
-</aside><?php /**PATH D:\laragon\www\upgradelaravel\hrm-payroll-v3-master\resources\views/administrator/layouts/left_side_bar.blade.php ENDPATH**/ ?>
--- storage/framework/views/7d9737c90e0c49dd9ecc61fed474e86d.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b><?php echo e(__('Version')); ?></b> 3.0.0
-    </div>
-    <strong><?php echo e(__('Copyright')); ?> &copy; 2021 <a href="https://amazingsoftbd.com" target="_blank"><?php echo e(__('Amazing Soft')); ?></a>.</strong> <?php echo e(__('All rights reserved.')); ?>
-
-</footer><?php /**PATH D:\laragon\www\upgradelaravel\hrm-payroll-v3-master\resources\views/administrator/layouts/footer.blade.php ENDPATH**/ ?>
